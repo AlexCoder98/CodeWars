996cfe38b921d1b8b5c2e2665ddf8e5d862603ef
Oleksandr Yurchuk <oleksandryurchuk@MacBook-Pro-Oleksandr.local>
2023-11-26 18:18:55 +0100

    comment added

--- 6 kyu/Count the smiley faces!/main.js
+++ 6 kyu/Count the smiley faces!/main.js
@@ -5,8 +5,8 @@ function countSmileys(arr) {
 
     // SECOND SOLUTION
     // const regExp2 = /[;:][-~]?[\)D]/g;
-    return arr.filter(el => el.match(regExp)).length;
+    // return arr.filter(el => el.match(regExp)).length;
 }
 
 const result = countSmileys([';]', ':[', ';*', ':$', ';-D']);
-// console.log(result);
+console.log(result);
