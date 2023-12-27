6bd8fbae1f344bb42632b38ff123202b9da0cf92
Oleksandr Yurchuk <oleksandryurchuk@MacBook-Pro-Oleksandr.local>
2023-12-27 22:15:00 +0100

    Clocky Mc Clock-Face kata is done

---
+++ 5 kyu/Regex Password Validation/main.js
@@ -0,0 +1,5 @@
+const password1 = "fjd3IR9";
+const password2 = "fjd3IR9.;";
+const password3 = "SJKHD23";
+const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{6,}$/;
+// console.log(regex.test(password));
